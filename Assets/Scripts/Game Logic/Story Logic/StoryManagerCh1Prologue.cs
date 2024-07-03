b618315b0b3e784595fef237a42113ecdebc5126
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-03 18:43:10 +0300

    Create Story Manager for Prologue Scene

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh1Prologue.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh1Prologue : MonoBehaviour
+    {
+        // Placeholder, logic will be added later on
+    }
+}
