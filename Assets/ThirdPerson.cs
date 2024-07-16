f6560d43cfd89c8a9c4bd54d0d55013f2a2f3610
ItzELECTR0 <82042993+ItzELECTR0@users.noreply.github.com>
2024-07-16 11:10:40 +0300

    Create ThirdPerson.cs
    Helper script for PlayerController.cs

---
+++ Assets/ThirdPerson.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class ThirdPerson : MonoBehaviour
+{
+    [Header("Variables")]
+    public Transform orientation;
+    public Transform Player;
+    public Transform playerObj;
+    [SerializeField] private Rigidbody rb;
+    public float roationSpeed;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+            
+    }
+}
+}
