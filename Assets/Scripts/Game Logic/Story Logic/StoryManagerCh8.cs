3ae3275660c03c40a7cf03422d1106390b066995
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:58 +0300

    Create StoryManagerCh8.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh8.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh8 : MonoBehaviour
+    {
+        public bool TUS3;
+        public bool TPC3;
+
+        // Static Silence;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
