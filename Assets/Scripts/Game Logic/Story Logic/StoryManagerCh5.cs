c065a3a0771c733cfcca4cc108cfc9c327795419
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:50 +0300

    Create StoryManagerCh5.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh5.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh5 : MonoBehaviour
+    {
+        public bool TUS2;
+        public bool TPC2;
+
+        // Surge Of Glory;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
