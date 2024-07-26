ff1fd4c41f31c98a62adb08fcc00b79cfba46843
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:56 +0300

    Create StoryManagerCh7.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh7.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh7 : MonoBehaviour
+    {
+        public bool TUS3;
+        public bool TPC3;
+
+        // One Shall Fall;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
