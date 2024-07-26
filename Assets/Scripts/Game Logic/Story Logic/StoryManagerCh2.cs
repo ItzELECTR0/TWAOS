6aeea6300573b774970972bd87de97a60f182e8e
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:42 +0300

    Create StoryManagerCh2.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh2 : MonoBehaviour
+    {
+        public bool TUS;
+        public bool TPC;
+
+        // Whispers of The Storm;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
