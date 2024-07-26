67f85cb358014a216a9d2632397bbec3cd3b7093
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:48:03 +0300

    Create StoryManagerCh10.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh10.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh10 : MonoBehaviour
+    {
+        public bool TUS5;
+        public bool TPC5;
+
+        // XXX XXXX XX XXX XXXXXX;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
