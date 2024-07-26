8923ad7d67558ecc81cbded603d45a8f4c627348
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:48:01 +0300

    Create StoryManagerCh9.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh9.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh9 : MonoBehaviour
+    {
+        public bool TUS4;
+        public bool TPC4;
+
+        // Electric Embers;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
