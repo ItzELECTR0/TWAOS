260e11e2bf648d9b37b48b008ea4206ffbbfde79
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:47 +0300

    Create StoryManagerCh4.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh4.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh4 : MonoBehaviour
+    {
+        public bool TUS1;
+        public bool TPC1;
+
+        // Scattered Sparks;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
