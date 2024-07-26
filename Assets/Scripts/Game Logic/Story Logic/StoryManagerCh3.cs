dd1833a65201f53348a446d7c7c4bbea954efd89
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:44 +0300

    Create StoryManagerCh3.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh3 : MonoBehaviour
+    {
+        public bool TUS1;
+        public bool TPC1;
+
+        // Thunder of The Reclaimed;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
