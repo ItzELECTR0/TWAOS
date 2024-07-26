179f69e448f61be92bf5b92654a3b036cbbc28f4
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:47:53 +0300

    Create StoryManagerCh6.cs

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh6.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh6 : MonoBehaviour
+    {
+        public bool TUS2;
+        public bool TPC2;
+
+        // One Shall Stand;
+        // This is a Placeholder, logic will be added later on;
+    }
+}
