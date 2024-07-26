d9308511ed41ef2b0f0747be8bf45da5cf3bbd8c
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-07-26 13:46:58 +0300

    Create StoryManagerCh0 scripts

---
+++ Assets/Scripts/Game Logic/Story Logic/StoryManagerCh0Paradox.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace ELECTRIS
+{
+    public class StoryManagerCh0Paradox : MonoBehaviour
+    {
+        // XXXX XXX XXXX XXX XXXXX?
+        // Placeholder, logic will be added later on
+    }
+}
