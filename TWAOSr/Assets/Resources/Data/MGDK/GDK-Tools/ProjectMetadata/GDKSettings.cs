e508503ecf81555f35066736525e4050e3711b5a
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-08-15 03:10:07 +0300

    Remove HoneyPhonic UI Soundpack

--- TWAOSr/Assets/Resources/Data/MGDK/GDK-Tools/ProjectMetadata/GDKSettings.cs
+++ TWAOSr/Assets/Resources/Data/MGDK/GDK-Tools/ProjectMetadata/GDKSettings.cs
@@ -1,8 +1,8 @@
 namespace ELECTRIS
 {
-public static class GdkPlatformSettings
-{
-    // A Microsoft Game Config (MGC) asset has not been configured for this platform (see GDK Settings).
-    // No idea how to get one yet
-}
+    public static class GdkPlatformSettings
+    {
+        // A Microsoft Game Config (MGC) asset has not been configured for this platform (see GDK Settings).
+        // No idea how to get one yet
+    }
 }
