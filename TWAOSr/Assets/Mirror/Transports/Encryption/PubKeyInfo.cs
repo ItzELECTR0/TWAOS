07e8318b01de7cd22ebafa3b0db130236c4b72bf
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2024-08-16 20:59:58 +0300

    Remove 'Mirror' asset package

--- TWAOSr/Assets/Mirror/Transports/Encryption/PubKeyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Org.BouncyCastle.Crypto;
-
-public struct PubKeyInfo
-{
-    public string Fingerprint;
-    public ArraySegment<byte> Serialized;
-    public AsymmetricKeyParameter Key;
-}
